122bbacbfa14a3a641ea78d724f19b06d7de2cce
René Müller <rene.mueller@clicksports.de>
2021-03-14 21:29:15 +0100

    routes/api.php: fixed typo

--- html/routes/api.php
+++ html/routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/coupons', 'Api\CouponController@index');
-Route::get('/coupon-category', 'Api\CouponCategoryController@index');
+Route::get('/coupon-categories', 'Api\CouponCategoryController@index');
 
