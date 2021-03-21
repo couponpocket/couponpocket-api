cc91a369a3d209792e7fdca9413094f5f7b27ba5
René Müller <rene.mueller@clicksports.de>
2021-03-21 14:10:34 +0100

    adjustments for CouponCategoryController.php: code clean up

--- html/app/Http/Controllers/Api/CouponCategoryController.php
+++ html/app/Http/Controllers/Api/CouponCategoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Core\Controllers\Api\ApiController;
 use App\Models\CouponCategory;
-use App\Models\Coupon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
