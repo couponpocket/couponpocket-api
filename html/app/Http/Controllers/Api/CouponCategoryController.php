d39cffee51b80bd9a8dba63c33607478848ef3e1
René Müller <rene.mueller@clicksports.de>
2021-05-08 14:22:29 +0200

    adjustments for CouponCategoryController.php: added where conditions for dates

--- html/app/Http/Controllers/Api/CouponCategoryController.php
+++ html/app/Http/Controllers/Api/CouponCategoryController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Core\Controllers\Api\ApiController;
 use App\Models\CouponCategory;
+use DateTime;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class CouponCategoryController extends ApiController
 {
@@ -17,8 +17,12 @@ class CouponCategoryController extends ApiController
      */
     public function index(Request $request): JsonResponse
     {
-        $sortCoupon = function (HasMany $q) {
-            $q->orderBy('condition', 'ASC')
+        $now = new DateTime();
+
+        $sortCoupon = function (HasMany $q) use ($now) {
+            $q->where('valid_from', '<=', $now->format('Y-m-d'))
+                ->where('valid_till', '>=', $now->format('Y-m-d'))
+                ->orderBy('condition', 'ASC')
                 ->orderByRaw('LENGTH(points) DESC, points DESC')
                 ->orderBy('valid_till', 'ASC');
         };
@@ -26,6 +30,10 @@ class CouponCategoryController extends ApiController
         return response()->json([
             'status' => 'true',
             'items' => CouponCategory::with(['coupons' => $sortCoupon])
+                ->whereHas('coupons', function ($q) use ($now) {
+                    $q->where('valid_from', '<=', $now->format('Y-m-d'))
+                        ->where('valid_till', '>=', $now->format('Y-m-d'));
+                })
                 ->orderBy('name', 'ASC')
                 ->get()
         ]);
