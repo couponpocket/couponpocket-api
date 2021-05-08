9b4011c2d39b415cf76eb3fe9c8d2fd0f197bbdc
René Müller <rene.mueller@clicksports.de>
2021-05-08 12:57:49 +0200

    adjustments for CouponCategoryController.php: improved sorting for coupons

--- html/app/Http/Controllers/Api/CouponCategoryController.php
+++ html/app/Http/Controllers/Api/CouponCategoryController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Core\Controllers\Api\ApiController;
 use App\Models\CouponCategory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CouponCategoryController extends ApiController
 {
@@ -15,12 +17,16 @@ class CouponCategoryController extends ApiController
      */
     public function index(Request $request): JsonResponse
     {
+        $sortCoupon = function (HasMany $q) {
+            $q->orderBy('condition', 'ASC')
+                ->orderByRaw('LENGTH(points) DESC, points DESC')
+                ->orderBy('valid_till', 'ASC');
+        };
+
         return response()->json([
             'status' => 'true',
-            'items' => CouponCategory::with(['coupons' => function ($q) {
-                $q->orderBy('condition', 'ASC')
-                    ->orderBy('points', 'DESC');
-            }])
+            'items' => CouponCategory::with(['coupons' => $sortCoupon])
+                ->orderBy('name', 'ASC')
                 ->get()
         ]);
     }
