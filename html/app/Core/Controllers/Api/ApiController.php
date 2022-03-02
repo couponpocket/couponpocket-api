9b89a3271e22531589b5d47d838b46fafd626a61
René Müller <mail@rene-mueller.xyz>
2022-03-02 17:53:50 +0100

    removed Controller.php and moved methods to ApiController.php

--- html/app/Core/Controllers/Api/ApiController.php
+++ html/app/Core/Controllers/Api/ApiController.php
@@ -2,8 +2,40 @@
 
 namespace App\Core\Controllers\Api;
 
-use App\Core\Controllers\Controller;
+use App\Core\Models\BaseModel;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
 
 class ApiController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected ?BaseModel $modelInUse = null;
+
+    /**
+     * define which main model this Controller is using
+     *
+     * @noinspection PhpDocFieldTypeMismatchInspection
+     * @var string|BaseModel $modelClass
+     */
+    protected string $modelClass;
+
+    /**
+     * @return BaseModel
+     */
+    protected function createModelFromClass(): BaseModel
+    {
+        return new $this->modelClass;
+    }
+
+    /**
+     * @param int $id
+     * @return BaseModel
+     */
+    protected function getModelById(int $id): BaseModel
+    {
+        return $this->modelClass::findOrFail($id);
+    }
 }
