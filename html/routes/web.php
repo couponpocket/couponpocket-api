08c3c06c7e1c2db8dd3c1479a42d5aecbb9fb6d3
René Müller <48084558+rene-mueller@users.noreply.github.com>
2022-08-23 08:46:29 +0200

    removed main web route

--- html/routes/web.php
+++ html/routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'app')->name('app');
-
 // email is sending an url for it:
 Route::get('/reset-password', function () {
     return redirect('/#/reset-password');
