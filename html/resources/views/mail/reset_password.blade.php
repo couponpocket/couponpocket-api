316c94c55da577f0b7d9c55da91ff3d3f6fd2d4c
René Müller <48084558+rene-mueller@users.noreply.github.com>
2022-08-23 08:52:58 +0200

    removed unnecessary views

--- html/resources/views/mail/reset_password.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>{{ __('mail.reset_password.headline') }}</h1>
-<p>{{ __('mail.reset_password.text1') }}</p>
-<a href="{{ $url }}">{{ __('mail.reset_password.button') }}</a>
-<p>{{ __('mail.reset_password.text2') }}</p>
